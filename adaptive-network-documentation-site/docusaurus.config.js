9177311e0d4f1fb202ea62d5067acc4a9cb94d21
Jack Walker <personal@jackwalker.co.nz>
2023-05-07 20:50:26 +1200

    ci: update edit urls for docusaurus

--- adaptive-network-documentation-site/docusaurus.config.js
+++ adaptive-network-documentation-site/docusaurus.config.js
@@ -42,14 +42,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/adaptive-network/documentation-site/tree/release/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/adaptive-network/documentation-site/tree/release/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
